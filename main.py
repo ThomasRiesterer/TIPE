00c19f207aa5886bbcdeb5569e861949ecc40525
ThomasRiesterer <thomas.riesterer@gmail.com>
2025-01-16 21:14:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,125 +18,90 @@ class Circle:
         self.vy = 0
         self.radius = radius
 
+    def on_goal(self):
+        return abs(self.x - goal[0]) <= 15 and abs(self.y - goal[1]) <= 15
+
     def render_tick(self):
-        pygame.draw.circle(screen, white, (self.x, self.y), self.radius/2, self.radius) 
+        pygame.draw.circle(screen, white, (self.x, self.y), self.radius, self.radius) 
         self.movement()
         self.x += self.vx
         self.y += self.vy
 
     def movement(self):
-        self.vx = 0
-        self.vy = 0
-        collisions, n = calc_collisions(self)
-
+        dx = goal[0] - self.x
+        dy = goal[1] - self.y
+        self.vx = (dx / (abs(dx) + abs(dy))) *speed
+        self.vy = (dy / (abs(dx) + abs(dy))) *speed
         collide(self)
-
-        if self.vx == 0 and self.vy == 0:
-            dx = goal[0] - self.x
-            dy = goal[1] - self.y
-            self.vx = (dx / (abs(dx) + abs(dy))) *speed
-            self.vy = (dy / (abs(dx) + abs(dy))) *speed
-            for c in collisions:
-                self.vx += c.vx
-                self.vy += c.vy
-            self.vx /= n
-            self.vy /= n
-            for c in collisions:
-                if not(self.x == c.x):
-                    self.vx += 1/(self.x - c.x) *coeff
-                if not(self.y == c.y):
-                    self.vy += 1/(self.y - c.y) *coeff
-
-        self.vx /= n
-        self.vy /= n
-
+        
 
 class Wall:
-    def __init__(self, start_x, start_y, end_x, end_y, group):
+    def __init__(self, start_x, start_y, end_x, end_y):
         self.start = (start_x, start_y)
         self.end = (end_x, end_y)
-        if self.start[0] == self.end[0] :
+        if self.start[0] == self.end[0]:
             self.orientation = 'V' # Vertical
-        elif self.start[1] == self.end[1] :
+        elif self.start[1] == self.end[1]:
             self.orientation = 'H' # Horizontal
-        else :
+        else:
             self.orientation = 'D' # Diagonal
-        self.group = group
     
     def render_tick(self):
         pygame.draw.line(screen, white, self.start, self.end)
 
 
-def on_goal(circle):
-    return abs(circle.x - goal[0]) <= 40 and abs(circle.y - goal[1]) <= 20
+class Object:
+    def __init__(self, center_h, center_v, size):
+        self.center_h = center_h
+        self.center_v = center_v
+        self.size = size
+        self.wall1 = Wall(self.center_h-self.size,self.center_v,self.center_h,self.center_v-self.size)
+        self.wall2 = Wall(self.center_h,self.center_v-self.size,self.center_h+self.size,self.center_v)
+        self.wall3 = Wall(self.center_h+self.size,self.center_v,self.center_h,self.center_v+self.size)
+        self.wall4 = Wall(self.center_h,self.center_v+self.size,self.center_h-self.size,self.center_v)
 
-def calc_collisions(circle):
-    collisions = []
-    for c in circles:
-        if c == circle:
-            continue
-        diff = sqrt((c.x - circle.x)**2 + (c.y - circle.y)**2)
-        dist = diff - (c.radius + circle.radius)
-        if dist < 0:
-            collisions.append(c)
-    n = 1+len(collisions)/3
-    return collisions, n
+    def render_tick(self):
+        pygame.draw.line(screen, white, self.wall1.start, self.wall1.end)
+        pygame.draw.line(screen, white, self.wall2.start, self.wall2.end)
+        pygame.draw.line(screen, white, self.wall3.start, self.wall3.end)
+        pygame.draw.line(screen, white, self.wall4.start, self.wall4.end)
 
-def wall_colliding_V(circle):
-    colliding = False
-    for w in walls:
-        if w.orientation == 'H':
-            continue
-        if (w.start[0] - circle.radius < circle.x and circle.x < w.end[0] + circle.radius):
-            if (w.start[1] < circle.y and circle.y < w.end[1]):
-                colliding = True
-                break
-    return colliding
-
-def wall_colliding_H(circle):
-    colliding = False
-    wall_group = 0
-    for w in walls:
-        if w.orientation == 'V':
-            continue
-        if (w.start[1] - circle.radius < circle.y and circle.y < w.end[1] + circle.radius):
-            if (w.start[0] < circle.x and circle.x < w.end[0]):
-                colliding = True
-                wall_group = w.group
-                break
-    return colliding, wall_group
-
-def wall_colliding_D(circle):
-    colliding = False
-    for w in walls:
-        if (w.start[0] < circle.x - circle.radius and circle.x + circle.radius < w.end[0]) or (w.end[0] < circle.x - circle.radius and circle.x + circle.radius < w.start[0]):
-            if (w.start[1] < circle.y - circle.radius and circle.y + circle.radius < w.end[1]) or (w.end[1] < circle.y - circle.radius and circle.y + circle.radius < w.start[1]):
-                colliding = True
-                break
-    return colliding
 
 def collide(circle):
-    colliding_H, colliding_group_H = wall_colliding_H(circle)
-    colliding_V = wall_colliding_V(circle)
-    colliding_D = wall_colliding_D(circle)
-    if colliding_H and colliding_group_H == 0:
-        if circle.x < goal[0]:
-            circle.vx = speed
-            circle.vy = 0
-        else:
-            circle.vx = -speed
-            circle.vy = 0
-    elif colliding_V:
-        circle.vx = 0
-        circle.vy = -speed
-    elif colliding_D:
-        normalized_speed = sqrt(circle.vx ** 2 + circle.vy ** 2)
-        if circle.x < goal[0]:
-            circle.vx = -normalized_speed
-            circle.vy = -normalized_speed
-        else:
-            circle.vx = normalized_speed
-            circle.vy = -normalized_speed
+    coeff_total = 1
+
+    for c in circles:
+        if circle == c:
+            continue
+        dist = sqrt((c.x - circle.x)**2 + (c.y - circle.y)**2)
+        if dist - (c.radius + circle.radius) < 0:
+            dx = c.x - circle.x
+            dy = c.y - circle.y
+            circle.vx -= (dx / (abs(dx) + abs(dy))) *speed
+            circle.vy -= (dy / (abs(dx) + abs(dy))) *speed
+            coeff_total *= coeff_ralentissement 
+
+    for w in walls:
+        if w.orientation == 'V' and (100 + circle.radius >= circle.x or circle.x >= 1180 - circle.radius):
+            circle.vx = 0
+        elif w.orientation == 'H' and (60 + circle.radius >= circle.y or circle.y >= 660 - circle.radius):
+            if not (610 + circle.radius < circle.x and circle.x < 670 - circle.radius):
+                circle.vy = 0
+
+    for o in objects:
+        normalized_speed = speed * sqrt(2)/2
+        dist_x = o.center_h - circle.x
+        dist_y = o.center_v - circle.y
+        if abs(dist_x) + abs(dist_y) - circle.radius < o.size:
+            if circle.x < o.center_h:
+                circle.vx = -normalized_speed
+                circle.vy = -normalized_speed
+            else:
+                circle.vx = normalized_speed
+                circle.vy = -normalized_speed
+
+    circle.vx *= coeff_total
+    circle.vy *= coeff_total
 
 
 ####################################################
@@ -149,9 +114,9 @@ white = (255,255,255)
 red = (255,0,0)
 black = (0,0,0)
 
-radius = 18
+radius = 10
 speed = 1
-coeff = 1.7
+coeff_ralentissement = 0.7
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -166,28 +131,28 @@ initial_time = time.time()
 ####################################################
 # Main run body
 
-nb = 50
+nb = 80
+#nb = 10
 circles = []
 for i in range(nb):
 #    circles.append(Circle(randint(120,1160), randint(80,640), radius))
 #    circles.append(Circle(randint(400,880), randint(250,500), radius))
-    circles.append(Circle(randint(520,760), randint(250,500), radius))    
+    circles.append(Circle(randint(340,940), randint(300,600), radius))    
 
 walls = []
-                                         # Object0
-walls.append(Wall(100,60,100,660,0))     # Left_Vertical
-walls.append(Wall(1180,60,1180,660,0))   # Right_Vertical
-walls.append(Wall(100,60,610,60,0))      # TopRight_Horizontal
-walls.append(Wall(670,60,1180,60,0))     # TopLeft_Horizontal
-walls.append(Wall(100,660,1180,660,0))   # Bottom_Horizontal
 
-o1_size = 40                             # Object1
-o1_cH = 640
-o1_cV = 150
-#walls.append(Wall(o1_cH-o1_size,o1_cV,o1_cH,o1_cV-o1_size,1))
-#walls.append(Wall(o1_cH,o1_cV-o1_size,o1_cH+o1_size,o1_cV,1))
-#walls.append(Wall(o1_cH+o1_size,o1_cV,o1_cH,o1_cV+o1_size,1))
-#walls.append(Wall(o1_cH,o1_cV+o1_size,o1_cH-o1_size,o1_cV,1))
+walls.append(Wall(100,60,100,660))        # Left_Vertical
+walls.append(Wall(1180,60,1180,660))      # Right_Vertical
+walls.append(Wall(100,60,610,60))         # TopRight_Horizontal
+walls.append(Wall(670,60,1180,60))        # TopLeft_Horizontal
+walls.append(Wall(100,660,1180,660))      # Bottom_Horizontal
+
+objects = []
+
+#objects.append(Object(640,180,70))       # One object
+
+objects.append(Object(570,180,40))        # Two objects
+objects.append(Object(710,180,40))
 
 
 run = True
@@ -200,9 +165,12 @@ while run:
     for w in walls:
         w.render_tick()
 
+    for o in objects:
+        o.render_tick()
+
     circles_new = []
     for c in circles:
-        if not(on_goal(c)):
+        if not(c.on_goal()):
             circles_new.append(c)
     circles = circles_new
         
